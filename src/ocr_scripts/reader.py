2996226bcaf1421c4ab7e13d39a5cbef1164ccaf
ViktorijaTilevska <viktorija.tilevska@samtrevano.ch>
2021-02-25 13:52:27 +0100

    update reader

--- src/ocr_scripts/reader.py
+++ src/ocr_scripts/reader.py
@@ -21,52 +21,48 @@ parser = argparse.ArgumentParser(usage="stats [-h] src dest lang name [-stats]")
 parser.add_argument('source', type=str, help='source file path, could also be a directory')
 parser.add_argument('-dest', default=".\scans_reader", type=str, help='output file path. Default directory: <.\scans>')
 parser.add_argument('-lang', default="eng", type=str, choices=['eng', 'ita'], help='the language. Choose between it(italian) or en(english). Default is en')
-parser.add_argument('-prefix', default=".\scan_1", type=str, help='output file name, if there are more files it defines the prefix')
+parser.add_argument('-prefix', default=".\scan", type=str, help='output file name, if there are more files it defines the prefix')
 
 args = parser.parse_args()
 
-source = args.source
-dest = args.dest
-lang = args.lang
-prefix = args.prefix
-
-def read(img_to_str):
-    txt = pytesseract.image_to_string(Image.open(img_to_str), lang=lang)
-    print("read file")
-    return txt
-
-def write_to_txt(dest):
-    #path = "{}/{}.txt".format(args.dest, args.prefix) #gestire il '/' !!
-    '''
-    if dest == ".\scans":
-        dest = ".\scans\{}.txt".format(args.prefix)
-    '''
-    
-    path = ".\scans_reader"
+# ---------------------------------------------------------
+# Legge una o più immagini e ne crive il contenuto nei file .txt.
+# 
+# source: il percorso della immagine da leggere
+# dest: il percorso dove vengono slvati i file
+# lang: il linguaggio in qui leggere l'immagine
+# prefix: il nome del file
+# ---------------------------------------------------------
+def write_to_txt(source, dest, lang, prefix):
+    text_to_write = pytesseract.image_to_string(Image.open(source), lang=lang)
 
     try:
-        os.mkdir(path)
+        os.mkdir(dest)
     except OSError:
-        print ("Creation of the directory %s failed" % path)
+        print ("The directory %s already exists" % dest)
     else:
-        print ("Successfully created the directory %s " % path)
-    
-    os. chdir(path)
-    
-    
-    with open(prefix + ".txt",'a+') as outfile:
-        outfile.write(read(source))    
-    
+        print ("Successfully created the directory %s " % dest)
 
-    '''
-    handle = open(dest + ".txt", "a+")
-    print("open file")
-
-    if handle.writable():
-        handle.write(read(source))
-        print("w file")
+    os.chdir(dest)
+    fileslist = os.listdir()
+    file_name = f"{prefix}.txt"
+    id = 1
+    out_file_name = prefix + f"({id}).txt"
+    print(fileslist)
     
-    handle.close()
-    '''
+    if not fileslist:
+        with open(file_name, 'w+') as outfile:
+            outfile.write(text_to_write)
 
-write_to_txt(args.dest)
+    
+    for f in fileslist:
+        if f == file_name:
+            with open(out_file_name, 'w+') as outfile:
+                outfile.write(text_to_write)
+                id += 1
+                print(id)
+        else:
+            with open(file_name, 'w+') as outfile:
+                outfile.write(text_to_write)
+          
+write_to_txt(args.source, args.dest, args.lang, args.prefix)
