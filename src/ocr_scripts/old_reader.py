aaa3f38c780d8c7d1e5e5d93e6169eca512fb6eb
ViktorijaTilevska <viktorija.tilevska@samtrevano.ch>
2021-03-25 15:37:37 +0100

    update reader.py

---
+++ src/ocr_scripts/old_reader.py
@@ -0,0 +1,177 @@
+# # -----------------------------------------------------------------------
+# # Controlla se ci sono più immagini come input
+# #
+# # source: il percorso delle immagini da leggere
+# # dest: il percorso dove verranno salvati i file
+# # lang: il linguaggio in qui leggere l'immagine
+# # prefix: il nome del file
+# # -----------------------------------------------------------------------
+# def multi_image(source, dest, lang, prefix):
+#     if len(source) > 1:
+#         for path in source:
+#             check_img_type(path, dest, lang, prefix)
+#     else:
+#         check_img_type(source, dest, lang, prefix)
+
+# # -----------------------------------------------------------------------
+# # Controlla se il formato dell'immagine passata dal utente è accettato.
+# #
+# # path: il percorso della immagine da leggere
+# # dest: il percorso dove verranno salvati i file
+# # lang: il linguaggio in qui leggere l'immagine
+# # prefix: il nome del file
+# # -----------------------------------------------------------------------
+# def check_img_type(source, dest, lang, prefix):
+#     image_ext = os.path.splitext(source)[-1]
+#     logging.debug(f"image ext: {image_ext}")
+#     if image_ext.lower() == ".png" or image_ext.lower() == ".jpg":
+#         create_output_file(source, dest, lang, prefix)
+#     else:       
+#         logging.info("Errore: Formato non accettato. Inserire immagini di tipo .png e/o .jpg")
+
+# # --------------------------------------------------------------------------
+# # Crea la cartella passata dall'utente se essa non esiste.
+# # Dopo averla creata, o se esiste già, entra nella cartella di destinazione.
+# #
+# # dest: il percorso dove verranno salvati i file
+# # --------------------------------------------------------------------------
+# def create_directory(dest):
+#     try:
+#         os.mkdir(dest)
+#     except OSError:
+#         logging.info(f"La cartella {dest} esiste gia")
+#     else:
+#         logging.info(f"La cartella {dest} e' stata creata")
+
+# # -----------------------------------------------------------
+# # Crea il file e scrive nell'esso il contenuto dell'immagine.
+# #
+# # file_name: il nome del file da creare
+# # text_to_write: il testo da inserire nel file 
+# # -----------------------------------------------------------
+# def write_file(file_name, text_to_write):
+#     with open(file_name, 'a+') as outfile:
+#         outfile.write(text_to_write)
+#         logging.info(f"Il file {file_name} e' stato creato")
+
+# -----------------------------------------------------------
+# Crea il file e scrive nell'esso il contenuto dell'immagine.
+#
+# file_name: il nome del file da creare
+# text_to_write: il testo da inserire nel file 
+# -----------------------------------------------------------
+# def write_list_of_text_in_file(file_name, list_of_text):
+#     with open(file_name, 'a+') as outfile:
+#         for text in list_of_text:
+#             outfile.write(text)
+#     logging.info(f"Il file {file_name} e' stato creato")
+
+# # -----------------------------------------------------------
+# # Se c'è un file con lo stesso nome dentro la cartella, crea il file aggiungendogli,
+# # alla fine, un numero in base al numero degli file con lo stesso nome.
+# # ex: scan.txt, scan_1.txt, scan_2.txt, ecc...
+# #
+# # file_name: il nome del file da creare
+# # text_to_write: il testo da inserire nel file 
+# # prefix: il nome del file
+# # -----------------------------------------------------------
+
+# def write_existing_file(file_name, text_to_write, prefix):
+#     # if len(source) is 1:
+#     existing_file_name = os.path.splitext(file_name)
+#     name_id = existing_file_name[0]
+#     id = name_id[-1]
+
+#     # e se ci sono 2 file che si chiamano 492850.png ? ->  492850_<>.png
+#     # 492851 -> 492850_1
+#     if id.isdigit():
+#         id = id + 1
+#     else:
+#         id = 1
+
+#     logging.debug(f"id: {id}")
+#     out_file_name = prefix + f"_{id}.txt"
+#     logging.debug(f"out name: {out_file_name}")
+
+#     write_file(out_file_name, text_to_write)
+#     # else:
+#     #     write_file(file_name, text_to_write)
+
+# # -----------------------------------------------------------
+# # Se c'è un file con lo stesso nome dentro la cartella, crea il file aggiungendogli,
+# # alla fine, un numero in base al numero degli file con lo stesso nome.
+# # ex: scan.txt, scan_1.txt, scan_2.txt, ecc...
+# #
+# # file_name: il nome del file da creare
+# # text_to_write: il testo da inserire nel file 
+# # prefix: il nome del file
+# # -----------------------------------------------------------
+# def write_list_of_text_in_existing_file(file_name, text_to_write, prefix):
+#     # if len(source) is 1:
+#     existing_file_name = os.path.splitext(file_name)
+#     name_id = existing_file_name[0]
+#     id = name_id[-1]
+
+#     # e se ci sono 2 file che si chiamano 492850.png ? ->  492850_<>.png
+#     # 492851 -> 492850_1
+#     if id.isdigit():
+#         id = id + 1
+#     else:
+#         id = 1
+
+#     logging.debug(f"id: {id}")
+#     out_file_name = prefix + f"_{id}.txt"
+#     logging.debug(f"out name: {out_file_name}")
+
+#     write_list_of_text_in_file(out_file_name, text_to_write)
+
+
+# # ---------------------------------------------------------
+# # Crea il file di output nella cartella specificata dall'utente,
+# # nella ligua specificata dall'utente e con il nome specificato dall'utente.
+# # 
+# # source: il percorso dell'immagine da leggere
+# # dest: il percorso dove verranno salvati i file
+# # lang: il linguaggio in qui leggere l'immagine
+# # prefix: il nome del file
+# # ---------------------------------------------------------
+# def create_output_file(source, dest, lang, prefix): 
+#     create_directory(dest)
+#     os.chdir(dest)
+#     logging.debug(f"source to open: {source}")
+#     logging.debug(f"img open source: {Image.open(source)}")
+#     file_name = f"{prefix}.txt"
+#     fileslist = os.listdir()
+#     # text_to_write = pytesseract.image_to_string(cv2.imread(source), lang)
+
+#     if len(source) > 1:
+#         logging.debug() 
+#         list_of_text = list()
+#         for path in source:
+#             try:
+#                 text_to_write = pytesseract.image_to_string(Image.open(source), lang)
+#                 list_of_text.append(text_to_write)
+#             except FileNotFoundError as fnf_error:
+#                 logging.error("Error: file {source} not found, {fnf_error} ")
+        
+#         if not fileslist:
+#             write_list_of_text_in_file(file_name, list_of_text)
+#         else:
+#             if (f in file_name for f in fileslist):
+#                 write_list_of_text_in_existing_file(file_name, text_to_write, prefix)
+#                 logging.debug(f"writes existing file")
+#             else:
+#                 write_list_of_text_in_file(file_name, text_to_write)
+#     else: 
+#         if not fileslist:
+#             write_file(file_name, text_to_write)
+#         else:
+#             # entra qua solo 1 volta
+#             # if all(f in file_name for f in fileslist):
+#             if (f in file_name for f in fileslist):
+#                 write_existing_file(file_name, text_to_write, prefix)
+#                 logging.debug(f"writes existing file")
+#             else:
+#                 write_file(file_name, text_to_write)
+            
+#     os.chdir("../")
--- src/ocr_scripts/reader.py
+++ src/ocr_scripts/reader.py
@@ -1,4 +1,5 @@
 import argparse, pytesseract, logging, sys, os.path
+from pathlib import Path
 from os import path
 try:
     from PIL import Image
@@ -12,18 +13,23 @@ author: Viktorija Tilevska, Thaisa De Torre
 version: 11.02.2012
 last change: 25.03.2021
 """
+
+# -----------------------------------------------------------------------
+# Fa lo scan di tutti i file validi. Se non ci sono file validi il programma finisce
+#
+# args: dizionario di argomenti da controllare
+# -----------------------------------------------------------------------
 def scan(args):
-    valid_files = check_source(args.source)
-    files_text = {} # files_text = { 'c:\cdscsdkl\img.png':'sopra la panca la capra campa', 'c:\miofile.png':'alsa fa la kalsa', ...}
-    #list_of_text = []
+    valid_files = validate_source(args.source)
+    files_text = {}
     if len(valid_files) != 0:
         for f in valid_files:
-            # è una dictionary/classe dove ad ogni file viene associato il contenuto dello stesso,
-            # si può anche associare il nome del file di output, delle statistiche (stats), filesize, 
-            # cronometrare il tempo di scansione per ogni file, ecc
             files_text[f] = {}
-            files_text[f]['txt'] = text_from_file(f, args.lang)
-            logging.info("testi e img associati e aggiunti a al dictionary")
+            files_text[f]['txt'] = get_text(f, args.lang)
+
+            #with open(args.prefix, 'w+') as outfile:
+            #    outfile.write(get_text(f, args.lang))
+            #logging.info(f"Il file {args.prefix} e' stato creato")
     else:
         sys.exit(1)
         logging.error("Program stopped. No valid files were inserted.")
@@ -31,293 +37,95 @@ def scan(args):
     # controllare se è una mark 
 
 # -----------------------------------------------------------------------
-# Controlla se gli elementi passati sono dei file o delle cartelle
+# Fa tutti i controlli e i cambiamenti in modo da avere una lista con solamente
+# i file validi da scannerizzare.
 #
-# source: la lista delle immagini da leggere
-#
-# test: è il metodo main
-# returns: ritorna la lista di file validi per la scansione 
+# source: lista con i percorsi sorgente
+# return: una lista con tutti i percorsi validi per l'ocr
 # -----------------------------------------------------------------------
-def check_source(source):
+def validate_source(source):
+    # quando metti una cartellanon riesce ad accedere i file, bisogna modificare il metodo check_permission()
     valid_files = [] #contiene tutti i file validi (jpg, png)
-
     file_list = len(source)
-    logging.info(f"count file: {file_list}")
+    logging.info(f"Number of files inserted: {file_list}")
 
     for img in source:
         if path.isfile(img):
-            logging.debug(f"file {img} e' un file")
-            
             if is_valid(img):
-                logging.debug("e' valido")
+                logging.debug(f"{img} is a valid file.")
                 valid_files.append(img)
-                logging.debug("aggiunge alla lista validi")
-
         elif path.isdir(img):
-            logging.debug("e' una dir")
-            dir_list = read_dir(img)
+            dir_list = get_dir_content(img)
             logging.debug(f"dir list: {dir_list}")
 
             for f in dir_list:
                 if is_valid(f):
                     valid_files.append(img)
 
-            logging.debug("tutti i file sono validi")
-            logging.debug("aggiunge alla lista validi quelli della cartella")
-
-    logging.debug(f"valid list: {valid_files}")
+    logging.debug(f"List of valid files ({len(valid_files)}): {valid_files}")
     return valid_files
 
 # -----------------------------------------------------------------------
-# Controlla se il formato dell'immagine è valido e se il file è accessibile. 
+# Controlla se il formato del file al percorso path sia accettato dall'ocr (png o jpg o jpeg). 
 #
-# src: il percorso dell'immagine
-# return: True se il file è valido, False se il file non è valido
-#
-# test: passare un'immagine con un formato diverso da .jpg e .png e 
-# vedere se appare il messaggio d'erroe, provare ad accedere a un'immagine
-# inaccessibile e vedere il messagio di erore che esce. 
+# src: percorso del file da controllare
+# return: true se il formato è accettato, altrimenti false
 # -----------------------------------------------------------------------            
 def is_valid(src):
     valid = False
     valid_extensions = ['.png', '.jpg', '.jpeg']
     file_ext = os.path.splitext(src)[-1]
-    logging.debug(f"file ext: {file_ext}")
-    if file_ext in valid_extensions and check_premissions(src):
-        logging.debug(f"checking file {src}")
+    if file_ext in valid_extensions and check_permission(src):
         valid = True
+        logging.debug(f"File {src} is valid")
     else:       
-        logging.debug("Errore: Formato non accettato. Inserire immagini di tipo .png e/o .jpg")
+        logging.error("Error: Format non accepted. Insert images PNG and/or JPG/JPEG")
         #sys.exit(1)
     return valid
 
 # --------------------------------------------------
 # Controlla se il file è accessibile in lettura
 #
-# src: il file da controllare 
-# returns: True se il file è accessibile, false se i fale non è accessibile
-#
-# test: passare un file non accesibile
+# path: il percorso del file da controllare 
+# returns: true se il file è accessibile, altrimenti ritorna false
 # -------------------------------------------------
-def check_premissions(src):
+def check_permission(path):
+    # migliorare questa parte
     valid = False
     try:
-        with open(src) as f:
-            logging.debug("lo legge")
+        with open(path) as f:
             valid = True
     except IOError:
-        logging.debug(f"Errore: il file {src} non è accessibile")
+        logging.debug(f"File {path} is not readable")
 
     return valid
 
 # ------------------------------------------------------------
-# Controlla se la cartella passata e vuota e se non lo è 
-# ne ritorna il contenuto
+# Prende e ritorna il contenuto della cartella nel percorso path.
 #
-# img: la cartella con delle immagini
-# returns: il contenuto della cartella
-#
-# test: passa una cartella e fai un print del contenuto, passa
-# una cartella vuota e guarda il messaggio d'erroe
+# path: percorso della cartella
+# returns: una lista con i file contenuti nella cartella
 # ------------------------------------------------------------
-def read_dir(img):
-    if len(os.listdir(img)) != 0:
-        return os.listdir(img)
+def get_dir_content(path):
+    if len(os.listdir(path)) != 0:
+        return os.listdir(path)
     else:
-        logging.info(f"Error: la cartella {img} e' vuota")
+        logging.debug(f"The directory {path} is empty")
         return None
 
 # -----------------------------------------------------------------------
-# Legge ogni immagine della lista e ne salva il cotenuto in un'altra lista 
+# Legge il contenuto di una immagine 
 #
-# f: il percorso del file valido
-# lang: la lingua -> farla una var globale se la voglio usare con tutti gli altri argomenti, fare una classe
-#
-# test: 
+# f: il percorso del file
+# lang: la lingua
+# return: una stringa con il contenuto dell'immagine
 # ----------------------------------------------------------------------- 
-def text_from_file(f, lang):
+def get_text(f, lang):
     try:
         text = pytesseract.image_to_string(Image.open(f), lang)
-        logging.debug("img scanned")
         return text
     except FileNotFoundError as fnf_error:
         logging.exception(f"Error: file {f} not found")
         return None
 
-# # -----------------------------------------------------------------------
-# # Controlla se ci sono più immagini come input
-# #
-# # source: il percorso delle immagini da leggere
-# # dest: il percorso dove verranno salvati i file
-# # lang: il linguaggio in qui leggere l'immagine
-# # prefix: il nome del file
-# # -----------------------------------------------------------------------
-# def multi_image(source, dest, lang, prefix):
-#     if len(source) > 1:
-#         for path in source:
-#             check_img_type(path, dest, lang, prefix)
-#     else:
-#         check_img_type(source, dest, lang, prefix)
-
-# # -----------------------------------------------------------------------
-# # Controlla se il formato dell'immagine passata dal utente è accettato.
-# #
-# # path: il percorso della immagine da leggere
-# # dest: il percorso dove verranno salvati i file
-# # lang: il linguaggio in qui leggere l'immagine
-# # prefix: il nome del file
-# # -----------------------------------------------------------------------
-# def check_img_type(source, dest, lang, prefix):
-#     image_ext = os.path.splitext(source)[-1]
-#     logging.debug(f"image ext: {image_ext}")
-#     if image_ext.lower() == ".png" or image_ext.lower() == ".jpg":
-#         create_output_file(source, dest, lang, prefix)
-#     else:       
-#         logging.info("Errore: Formato non accettato. Inserire immagini di tipo .png e/o .jpg")
-
-# # --------------------------------------------------------------------------
-# # Crea la cartella passata dall'utente se essa non esiste.
-# # Dopo averla creata, o se esiste già, entra nella cartella di destinazione.
-# #
-# # dest: il percorso dove verranno salvati i file
-# # --------------------------------------------------------------------------
-# def create_directory(dest):
-#     try:
-#         os.mkdir(dest)
-#     except OSError:
-#         logging.info(f"La cartella {dest} esiste gia")
-#     else:
-#         logging.info(f"La cartella {dest} e' stata creata")
-
-# # -----------------------------------------------------------
-# # Crea il file e scrive nell'esso il contenuto dell'immagine.
-# #
-# # file_name: il nome del file da creare
-# # text_to_write: il testo da inserire nel file 
-# # -----------------------------------------------------------
-# def write_file(file_name, text_to_write):
-#     with open(file_name, 'a+') as outfile:
-#         outfile.write(text_to_write)
-#         logging.info(f"Il file {file_name} e' stato creato")
-
-# # -----------------------------------------------------------
-# # Crea il file e scrive nell'esso il contenuto dell'immagine.
-# #
-# # file_name: il nome del file da creare
-# # text_to_write: il testo da inserire nel file 
-# # -----------------------------------------------------------
-# def write_list_of_text_in_file(file_name, list_of_text):
-#     with open(file_name, 'a+') as outfile:
-#         for text in list_of_text:
-#             outfile.write(text)
-#     logging.info(f"Il file {file_name} e' stato creato")
-
-# # -----------------------------------------------------------
-# # Se c'è un file con lo stesso nome dentro la cartella, crea il file aggiungendogli,
-# # alla fine, un numero in base al numero degli file con lo stesso nome.
-# # ex: scan.txt, scan_1.txt, scan_2.txt, ecc...
-# #
-# # file_name: il nome del file da creare
-# # text_to_write: il testo da inserire nel file 
-# # prefix: il nome del file
-# # -----------------------------------------------------------
-
-# def write_existing_file(file_name, text_to_write, prefix):
-#     # if len(source) is 1:
-#     existing_file_name = os.path.splitext(file_name)
-#     name_id = existing_file_name[0]
-#     id = name_id[-1]
-
-#     # e se ci sono 2 file che si chiamano 492850.png ? ->  492850_<>.png
-#     # 492851 -> 492850_1
-#     if id.isdigit():
-#         id = id + 1
-#     else:
-#         id = 1
-
-#     logging.debug(f"id: {id}")
-#     out_file_name = prefix + f"_{id}.txt"
-#     logging.debug(f"out name: {out_file_name}")
-
-#     write_file(out_file_name, text_to_write)
-#     # else:
-#     #     write_file(file_name, text_to_write)
-
-# # -----------------------------------------------------------
-# # Se c'è un file con lo stesso nome dentro la cartella, crea il file aggiungendogli,
-# # alla fine, un numero in base al numero degli file con lo stesso nome.
-# # ex: scan.txt, scan_1.txt, scan_2.txt, ecc...
-# #
-# # file_name: il nome del file da creare
-# # text_to_write: il testo da inserire nel file 
-# # prefix: il nome del file
-# # -----------------------------------------------------------
-# def write_list_of_text_in_existing_file(file_name, text_to_write, prefix):
-#     # if len(source) is 1:
-#     existing_file_name = os.path.splitext(file_name)
-#     name_id = existing_file_name[0]
-#     id = name_id[-1]
-
-#     # e se ci sono 2 file che si chiamano 492850.png ? ->  492850_<>.png
-#     # 492851 -> 492850_1
-#     if id.isdigit():
-#         id = id + 1
-#     else:
-#         id = 1
-
-#     logging.debug(f"id: {id}")
-#     out_file_name = prefix + f"_{id}.txt"
-#     logging.debug(f"out name: {out_file_name}")
-
-#     write_list_of_text_in_file(out_file_name, text_to_write)
-
-
-# # ---------------------------------------------------------
-# # Crea il file di output nella cartella specificata dall'utente,
-# # nella ligua specificata dall'utente e con il nome specificato dall'utente.
-# # 
-# # source: il percorso dell'immagine da leggere
-# # dest: il percorso dove verranno salvati i file
-# # lang: il linguaggio in qui leggere l'immagine
-# # prefix: il nome del file
-# # ---------------------------------------------------------
-# def create_output_file(source, dest, lang, prefix): 
-#     create_directory(dest)
-#     os.chdir(dest)
-#     logging.debug(f"source to open: {source}")
-#     logging.debug(f"img open source: {Image.open(source)}")
-#     file_name = f"{prefix}.txt"
-#     fileslist = os.listdir()
-#     # text_to_write = pytesseract.image_to_string(cv2.imread(source), lang)
-
-#     if len(source) > 1:
-#         logging.debug() 
-#         list_of_text = list()
-#         for path in source:
-#             try:
-#                 text_to_write = pytesseract.image_to_string(Image.open(source), lang)
-#                 list_of_text.append(text_to_write)
-#             except FileNotFoundError as fnf_error:
-#                 logging.error("Error: file {source} not found, {fnf_error} ")
-        
-#         if not fileslist:
-#             write_list_of_text_in_file(file_name, list_of_text)
-#         else:
-#             if (f in file_name for f in fileslist):
-#                 write_list_of_text_in_existing_file(file_name, text_to_write, prefix)
-#                 logging.debug(f"writes existing file")
-#             else:
-#                 write_list_of_text_in_file(file_name, text_to_write)
-#     else: 
-#         if not fileslist:
-#             write_file(file_name, text_to_write)
-#         else:
-#             # entra qua solo 1 volta
-#             # if all(f in file_name for f in fileslist):
-#             if (f in file_name for f in fileslist):
-#                 write_existing_file(file_name, text_to_write, prefix)
-#                 logging.debug(f"writes existing file")
-#             else:
-#                 write_file(file_name, text_to_write)
-            
-#     os.chdir("../")
+#https://docs.python.org/3/library/os.path.html link utile per lavorare con i percorsi
